abfd940d2204a8c282f0c1149a6895eeefe54318
Brandon Maxwell <brandon.l.maxwell@gmail.com>
2021-08-20 15:55:04 -0500

    updated resume and added hover to side nav to show contact info

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,15 +17,15 @@ function Header() {
             <h1 id="primaryH1">Brandon Maxwell</h1><br/>
             <a className="nav-link" href="#about">About</a>
             <a className="nav-link" href="#work">Work</a>
-            <a className href={Resume}>Resume</a>
+            <a className href={Resume} target="_blank">Resume</a>
             <br/>
-            <a className="nav-link" href="tel:+14176936331"><img src={Phone} alt="phone"
+            <a className="nav-link" data-toggle="popover" title="(417)693-6331" href="tel:+14176936331"><img src={Phone} alt="phone"
+                    height="30px"/></a> 
+            <a className="nav-link" data-toggle="popover" title="brandon.l.maxwell@gmail.com" href="mailto:brandon.l.maxwell@gmail.com"><img src={Email} alt="email"
                     height="30px"/></a>
-            <a className="nav-link" href="mailto:brandon.l.maxwell@gmail.com"><img src={Email} alt="email"
-                    height="30px"/></a>
-            <a className="nav-link" href="https://github.com/brandon-maxwell"><img src={GitHub}
+            <a className="nav-link" data-toggle="popover" title="https://github.com/brandon-maxwell" href="https://github.com/brandon-maxwell" target="_blank"><img src={GitHub}
                     alt="GitHub" height="30px"/></a>
-            <a className="nav-link" id="linkedin" href="https://www.linkedin.com/in/brandon-maxwell-b307b955/"><img
+            <a className="nav-link" data-toggle="popover" title="https://www.linkedin.com/in/brandon-maxwell-b307b955/" id="linkedin" href="https://www.linkedin.com/in/brandon-maxwell-b307b955/" target="_blank"><img
                     src={LinkedIn} alt="LinkedIn" height="30px"/></a>
         </div>
 
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -12,7 +12,7 @@ function Project() {
                         <div className="hoverTag">
                             <h4>Growr</h4>
                             <h5>MERN-Stack</h5>
-                            <a href="https://growr-project.herokuapp.com/" className="waves-effect waves-light btn-small">Visit Site</a>
+                            <a href="https://growr-project.herokuapp.com/" className="waves-effect waves-light btn-small">Visit Site</a> <br/>
                             <a href="https://github.com/brandon-maxwell/growr-plant-exchange" className="waves-effect waves-light btn-small">See on GitHub</a>
                         </div>
                     </div>
@@ -22,7 +22,7 @@ function Project() {
                         <div className="hoverTag">
                         <h4>Kiskis</h4>
                             <h5>Full-Stack</h5>
-                            <a href="https://lit-escarpment-01189.herokuapp.com/" className="waves-effect waves-light btn-small">Visit Site</a>
+                            <a href="https://lit-escarpment-01189.herokuapp.com/" className="waves-effect waves-light btn-small">Visit Site</a> <br/>
                             <a href="https://github.com/brandon-maxwell/kiskis" className="waves-effect waves-light btn-small">See on GitHub</a>
                         </div>
                     </div>
@@ -31,7 +31,7 @@ function Project() {
                             <div className="hoverTag">
                             <h4>Brew Me</h4>
                             <h5>JavaScript</h5>
-                            <a href="https://brandon-maxwell.github.io/BREW_ME/" className="waves-effect waves-light btn-small">Visit Site</a>
+                            <a href="https://brandon-maxwell.github.io/BREW_ME/" className="waves-effect waves-light btn-small">Visit Site</a> <br/>
                             <a href="https://github.com/brandon-maxwell/BREW_ME" className="waves-effect waves-light btn-small">See on GitHub</a>
                             </div>
                         </div>
@@ -42,7 +42,7 @@ function Project() {
                             <div className="hoverTag">
                                 <h4>Employee Directory</h4>
                                 <h5>Full-Stack with React</h5>
-                                <a href="https://brandon-maxwell.github.io/Employee-Directory/" className="waves-effect waves-light btn-small">Visit Site</a>
+                                <a href="https://brandon-maxwell.github.io/Employee-Directory/" className="waves-effect waves-light btn-small">Visit Site</a> <br/>
                                 <a href="https://github.com/brandon-maxwell/Employee-Directory" className="waves-effect waves-light btn-small">See on GitHub</a>
                             </div>
                         </div>
@@ -52,19 +52,9 @@ function Project() {
                                 <div className="hoverTag">
                                     <h4>Fitness Tracker</h4>
                                     <h5>JavaScript / MongoDB</h5>
-                                    <a href="https://aqueous-springs-60277.herokuapp.com/" className="waves-effect waves-light btn-small">Visit Site</a>
+                                    <a href="https://aqueous-springs-60277.herokuapp.com/" className="waves-effect waves-light btn-small">Visit Site</a> <br/>
                                     <a href="https://github.com/brandon-maxwell/Workout_Tracker" className="waves-effect waves-light btn-small">See on GitHub</a>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="flex-item Project-6 col s12 m12 l10 offset-l1">
-                            <div className="hoverTag">
-                                <h4>Weather Dashboard</h4>
-                                <h5>JavaScript</h5>
-                                <a href="https://brandon-maxwell.github.io/Weather-Dashboard/" className="waves-effect waves-light btn-small">Visit Site</a>
-                                <a href="https://github.com/brandon-maxwell/Weather-Dashboard" className="waves-effect waves-light btn-small">See on GitHub</a>
                             </div>
                         </div>
                     </div>
